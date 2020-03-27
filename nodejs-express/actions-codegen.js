6cd026a40af5aae8547bdecdfb3b1a45d35d8927
Steve Phillips <steve@tryingtobeawesome.com>
2020-03-27 01:39:22 -0700

    actions-codegen.js: missing "else" in conditional (#6)
    * actions-codegen.js: missing "else" in conditional
    
    * make the same fix for other frameworks
    
    Co-authored-by: Rishichandra Wawhal <rishi@hasura.io>

--- nodejs-express/actions-codegen.js
+++ nodejs-express/actions-codegen.js
@@ -12,7 +12,7 @@ const templater = (actionName, actionsSdl, derive) => {
         mutationDef = d.fields.find(f => f.name.value === actionName);
         if (!mutationDef) {
           return false;
-        } {
+        } else {
           return true;
         }
       }
