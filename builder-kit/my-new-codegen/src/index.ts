fe83a92aac22e23a22f3c4cf4fa4065e0da73f77
Gavin <ray.gavin97@gmail.com>
2020-04-23 00:39:11 -0400

    Make index.ts for published Codegen use same (working) code as debug

--- builder-kit/my-new-codegen/src/index.ts
+++ builder-kit/my-new-codegen/src/index.ts
@@ -1,10 +1,10 @@
 import {
-  JSDocTypeConverter,
-  TSTypeConverter,
-  GoTypeConverter,
-  PythonTypeConverter,
-  KotlinTypeConverter,
-} from './languages'
+  graphqlSchemaToJSDoc,
+  graphqlSchemaToTypescript,
+  graphqlSchemaToGo,
+  graphqlSchemaToPython,
+  graphqlSchemaToKotlin,
+} from './languages-functional'
 
 import {
   javascriptExpressTemplate,
@@ -16,62 +16,51 @@ import {
 } from './templates'
 
 import { buildActionTypes } from './schemaTools'
+import { DeriveParams } from './types'
 
-const templater = (actionName, actionSdl, derive) => {
+const templater = (
+  actionName: string,
+  actionSdl: string,
+  derive: DeriveParams | null
+) => {
   const actionParams = buildActionTypes(actionName, actionSdl)
   const templateParams = { ...actionParams, derive }
-
   /**
    * Javascript
    */
-  const jsTypeConverter = new JSDocTypeConverter({
-    schema: actionSdl,
-  })
   const jsCodegen = javascriptExpressTemplate({
     ...templateParams,
-    typeDefs: jsTypeConverter.generateTypes(),
+    typeDefs: graphqlSchemaToJSDoc(actionSdl),
   })
 
   /**
    * Typescript
    */
-  const tsTypeConverter = new TSTypeConverter({
-    schema: actionSdl,
-  })
   const tsCodegen = typescriptExpressTemplate({
     ...templateParams,
-    typeDefs: tsTypeConverter.generateTypes(),
+    typeDefs: graphqlSchemaToTypescript(actionSdl),
   })
 
   /**
    * Go
    */
-  const goTypeConverter = new GoTypeConverter({
-    schema: actionSdl,
-  })
   const goCodegen = goServeMuxTemplate({
     ...templateParams,
-    typeDefs: goTypeConverter.generateTypes(),
+    typeDefs: graphqlSchemaToGo(actionSdl),
   })
 
   /**
    * Python
    */
-  const pythonTypeConverter = new PythonTypeConverter({
-    schema: actionSdl,
-  })
   const pythonCodeGen = pythonFastAPITemplate({
     ...templateParams,
-    typeDefs: pythonTypeConverter.generateTypes(),
+    typeDefs: graphqlSchemaToPython(actionSdl),
   })
 
   /**
    * Kotlin
    */
-  const kotlinTypeConverter = new KotlinTypeConverter({
-    schema: actionSdl,
-  })
-  const kotlinTypes = kotlinTypeConverter.generateTypes()
+  const kotlinTypes = graphqlSchemaToKotlin(actionSdl)
   const kotlinHttp4kCodegen = kotlinHttp4kTemplate({
     ...templateParams,
     typeDefs: kotlinTypes,
