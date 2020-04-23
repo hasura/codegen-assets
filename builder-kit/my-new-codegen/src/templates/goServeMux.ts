27307366c413baddf6a610a65ad0871693b26ff0
Rishichandra Wawhal <rishi@hasura.io>
2020-04-23 19:06:24 +0530

    fully working golang codegen

--- builder-kit/my-new-codegen/src/templates/goServeMux.ts
+++ builder-kit/my-new-codegen/src/templates/goServeMux.ts
@@ -1,6 +1,6 @@
 import { html as template } from 'common-tags'
 import { ITypeNode, CodegenTemplateParams } from '../types'
-import { getRootFieldName } from '../utils';
+import { getRootFieldName, capitalize } from '../utils';
 
 const sampleValues = {
   'Int': 1111,
@@ -16,62 +16,84 @@ export const goServeMuxTemplate = (params: CodegenTemplateParams) => {
   const returnTypeDef = types[returnType]
 
   let delegationTypedefs = derive ? template`
-    
+
     type GraphQLRequest struct {
-      query string
-      variables CustomInsertUserArgs
+      Query     string               \`json:"query"\`
+      Variables ${actionName}Args \`json:"variables"\`
     }
-
     type GraphQLData struct {
-      ${getRootFieldName(derive.operation)} CustomInsertUserOutput
+      ${getRootFieldName(derive.operation, true)} ${returnType} \`json:"${getRootFieldName(derive.operation)}"\`
     }
-
-    type GraphQLError struct {
-      message string
-    }
-
     type GraphQLResponse struct {
-      data GraphQLData
-      error []GraphQLError
+      Data   GraphQLData    \`json:"data,omitempty"\`
+      Errors []GraphQLError \`json:"errors,omitempty"\`
     }
   ` : '';
 
   let executeFunc = derive ? template`
-    func execute (variables: CustomInsertUserArgs) (response ${returnType}, err Error) {
-      reqBody := GraphQLRequest {
-        query: "${derive.operation}",
-        variables: variables
+    func execute(variables ${actionName}Args) (response GraphQLResponse, err error) {
+
+      // build the request body
+      reqBody := GraphQLRequest{
+        Query:     "${derive.operation}",
+        Variables: variables,
       }
       reqBytes, err := json.Marshal(reqBody)
       if err != nil {
         return
       }
-      respBytes, err := http.Post("${derive.endpoint}", "application/json", bytes.NewBuffer(reqBytes))
+
+      // make request to Hasura
+      resp, err := http.Post("http://localhost:8080/v1/graphql", "application/json", bytes.NewBuffer(reqBytes))
       if err != nil {
         return
       }
-      hasuraResponse, err := json.Unmarshal(respBytes, GraphQLResponse)
+
+      // parse the response
+      respBytes, err := ioutil.ReadAll(resp.Body)
       if err != nil {
         return
       }
-      response := hasuraResponse.data.${getRootFieldName(derive.operation)}
-      return;
+      err = json.Unmarshal(respBytes, &response)
+      if err != nil {
+        return
+      }
+
+      // return the response
+      return
     }
+
   ` : '';
 
   let handlerFunc = derive ? template`
     // Auto-generated function that takes the Action parameters and must return it's response type
-    func ${actionName}(args ${actionName}Args) (response ${returnType}, err Error) {
-      response, err := execute(args)
+    func ${actionName}(args ${actionName}Args) (response ${returnType}, err error) {
+
+      hasuraResponse, err := execute(args)
+
+      // throw if any unexpected error happens
+      if err != nil {
+        return
+      }
+
+      // delegate Hasura error
+      if len(hasuraResponse.Errors) != 0 {
+        err = errors.New(hasuraResponse.Errors[0].Message)
+        return
+      }
+
+      response = hasuraResponse.Data.${getRootFieldName(derive.operation, true)}
       return
-    }   
+
+    }
+
   ` : template`
     // Auto-generated function that takes the Action parameters and must return it's response type
-    func ${actionName}(args ${actionName}Args) (${returnType}, Error) {
-      response := ${returnType} {
+    func ${actionName}(args ${actionName}Args) (response ${returnType}, err error) {
+      response =  ${returnType} {
         ${returnTypeDef.map(f => {
-          return `${f.name}: ${sampleValues[f.type.name]}`
-        }).join(',\n')}
+          return `${capitalize(f.name)}: ${sampleValues[f.type.name]}`
+        }).join(',\n')},
       }
       return response, nil
     }
@@ -82,38 +104,64 @@ export const goServeMuxTemplate = (params: CodegenTemplateParams) => {
     package main
 
     import (
+      "bytes"
       "encoding/json"
+      "io/ioutil"
       "log"
       "net/http"
-    )      
+    )
 
     ${typeDefs}
-    ${delegationTypedefs}
- 
-    func handler(w http.ResponseWriter, r *http.Request) {
-      // Declare a new struct for unmarshalling the arguments
-      var actionParams ${actionName}Args
 
-      // Try to decode the request body into the struct. If there is an error,
-      // respond to the client with the error message and a 400 status code.    
-      err := json.NewDecoder(r.Body).Decode(&actionParams)
+    type ActionPayload struct {
+      SessionVariables map[string]interface{} \`json:"session_variables"\`
+      Input            ${actionName}Args \`json:"input"\`
+    }
+
+    type GraphQLError struct {
+      Message string \`json:"message"\`
+    }
+
+    ${delegationTypedefs}
+    
+    func handler(w http.ResponseWriter, r *http.Request) {
+
+      // set the response header as JSON
+      w.Header().Set("Content-Type", "application/json")
+
+      // read request body
+      reqBody, err := ioutil.ReadAll(r.Body)
       if err != nil {
-        http.Error(w, err.Error(), http.StatusBadRequest)
+        http.Error(w, "invalid payload", http.StatusBadRequest)
         return
       }
-      
-      // Send the request params to the Action's generated handler function
-      result, err := ${actionName}(actionParams)
-      data, err := json.Marshal(result)
+
+      // parse the body as action payload
+      var actionPayload ActionPayload
+      err = json.Unmarshal(reqBody, &actionPayload)
       if err != nil {
-        http.Error(w, err.Error(), http.StatusBadRequest)
+        http.Error(w, "invalid payload", http.StatusBadRequest)
+        return
+      }
+
+      // Send the request params to the Action's generated handler function
+      result, err := ${actionName}(actionPayload.Input)
+
+      // throw if an error happens
+      if err != nil {
+        errorObject := GraphQLError{
+          Message: err.Error(),
+        }
+        errorBody, _ := json.Marshal(errorObject)
+        w.WriteHeader(http.StatusBadRequest)
+        w.Write(errorBody)
         return
       }
 
       // Write the response as JSON
-      w.WriteHeader(http.StatusOK)
-      w.Header().Set("Content-Type", "application/json")
+      data, _ := json.Marshal(result)
       w.Write(data)
+
     }
 
     ${handlerFunc}
@@ -124,7 +172,7 @@ export const goServeMuxTemplate = (params: CodegenTemplateParams) => {
       mux := http.NewServeMux()
       mux.HandleFunc("/${actionName}", handler)
 
-      err := http.ListenAndServe(":8080", mux)
+      err := http.ListenAndServe(":3000", mux)
       log.Fatal(err)
     }
   `
