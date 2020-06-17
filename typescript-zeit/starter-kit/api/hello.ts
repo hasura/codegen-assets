7ead0047ae634ec9e08ff470cc837f2102794679
Dipak Parmar <hi@dipak.tech>
2020-06-17 14:16:21 -0700

    Removed zeit verison of typescript-zeit

--- typescript-zeit/starter-kit/api/hello.ts
+++
@@ -1,8 +0,0 @@
-import { NowRequest, NowResponse } from '@now/node'
-
-const handler = (req: NowRequest, resp: NowResponse) => {
-  // You can access the request body at req.body
-  return resp.json({ "hello": "world" });
-};
-
-export default handler;
